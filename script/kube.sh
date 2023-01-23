eeffb3174f7b5bbc05513e41e4a60aa6d5b648f2
santhoshkumarg25 <116114085+santhoshkumarg25@users.noreply.github.com>
2023-01-23 13:28:18 +0530

    Create kube.sh

---
+++ script/kube.sh
@@ -0,0 +1,17 @@
+sudo apt-get update
+sudo apt-get install -y apt-transport-https gnupg2
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a
+/etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+#sudo apt-get install -y kubernetes-cni
+sudo apt-get install -y kubelet=1.20.1-00 kubeadm=1.20.1-00 kubectl=1.20.1-00
+docker.io
+sudo apt-mark hold kubelet kubeadm kubectl
+sudo systemctl enable docker
+sudo kubeadm config images pull
+echo '{"exec-opts": ["native.cgroupdriver=systemd"]}' | sudo tee
+/etc/docker/daemon.json
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+sudo systemctl restart kubelet
